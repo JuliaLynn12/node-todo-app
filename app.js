b7109b3cc59a081c6a7e8fac85c0d3e46822e9b3
Julia <julialynn396@gmail.com>
2021-05-13 15:18:41 -0500

    adding todo.js file and creating todo class

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ mongoose.connect("mongodb://localhost/todo_express", {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.listen(3000, () => console.log("Server started listening on port: 3000"));
