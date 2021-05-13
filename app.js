01e42e2bdc7f4ac14094899bea33ae41f14bd812
Julia <julialynn396@gmail.com>
2021-05-13 15:01:03 -0500

    connecting to mongodb

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost/todo_express", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.listen(3000, () => console.log("Server started listening on port: 3000"));
