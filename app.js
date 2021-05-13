939f1af666f76ef16200277905afaa9560152fd8
Julia <julialynn396@gmail.com>
2021-05-13 15:45:36 -0500

    adding anoother route

--- app.js
+++ app.js
@@ -12,4 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.use(require("./routes/index"))
+
 app.listen(3000, () => console.log("Server started listening on port: 3000"));
